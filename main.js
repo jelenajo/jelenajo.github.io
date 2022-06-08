6d60544a613b063d62279b8a8cabe1c736170a7d
Jelena Jovanović <69915994+jelenajo@users.noreply.github.com>
2022-06-08 23:20:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,246 @@
+$(document).ready(function() {
+    $("#headline").css(
+      "background-color", "gray");
+});
+
+
+
+$(document).ready(function(){
+  $(".ch").css(
+    "background-color", "#66e0ff"
+  );
+});
+$(document).ready(function(){
+  $("#mc>td:last-child").css(
+    "background-color", "#ffb3b3"
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:first-child").css(
+    "background-color", "#ff8080"
+    
+  );
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(5)").css(
+    "background-color", "red"
+  );
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(2)").css(
+    "background-color", "#4d4dff"
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(3)").css(
+    "background-color", "#66ffb3"
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(4").css(
+    "background-color", "#001a0d"
+  );
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(6)").css(
+    "background-color", " #ff33cc"
+
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(7)").css(
+    "background-color", "#999966"
+
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(8)").css(
+    "background-color", "#ff794d"
+
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(9)").css(
+    "background-color", "#006666"
+
+  );
+
+});
+$(document).ready(function(){
+  $("#mc>td:nth-child(10)").css(
+    "background-color", "#134d00"
+
+  );
+
+});
+$(document).ready(function(){
+  $("#ev").css(
+    "background-color", "#ffff4d"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".ars").css(
+    "background-color", "#b3b3ff"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:first-child").css(
+    "background-color", "#5c8a8a"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:last-child").css(
+    "background-color", "#008000"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(2)").css(
+    "background-color", "#cc33ff"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(3)").css(
+    "background-color", "yellow"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(4)").css(
+    "background-color", "#d9d9d9"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(5)").css(
+    "background-color", "#ffb3ff"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(6)").css(
+    "background-color", "#ff0080"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(7)").css(
+    "background-color", "#ffcc99"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(8)").css(
+    "background-color", "#00ffff"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(9)").css(
+    "background-color", "#cc8800"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".we>td:nth-child(10)").css(
+    "background-color", "#bf80ff"
+
+  );
+
+});
+$(document).ready(function(){
+  $(".fu").css(
+    "background-color", "#ffff99"
+  );
+
+});
+$(document).ready(function(){
+  $("#li").css(
+    "background-color", "#80ff80",
+  
+  );
+
+});
+$(document).ready(function(){
+  $(".sw").hover(function(){
+    $(this).css("background-color", " #800080");
+    }, function(){
+    $(this).css("background-color", "olive");
+  });
+});
+$(document).ready(function(){
+    $(".re").animate({
+      width: "120px",
+      height: "120px",
+      opacity: "0.8",
+      color: "red"
+    });
+
+  });
+
+ $(document).ready(function(){
+   $(".re").css("background-color", "red");
+ });
+ $(document).ready(function(){
+  $(".re>td:nth-child(5)").css(
+    "background-color", "blue");
+
+ });
+
+
+  $(document).ready(function(){
+    $(".re").hover(function(){
+      $(this).css("background-color", "gray");
+      }, function(){
+      $(this).css("background-color", "plum");
+    });
+  });
+  $(document).ready(function(){
+    $(".pa").css(
+      "background-color", "#80ff80",
+    
+    );
+  
+  });
+  $(document).ready(function(){
+    $(".as").css(
+      "background-color", "#80ff80",
+    
+    );
+  
+  });
+
+$(document).ready(function() {
+  $("#head").css(
+    "background-color", "#ffcccc");
+});
+$(document).ready(function(){
+  $("#head").animate({
+    backgroundColor : "yellow",
+    
+  }, 2000)
+    
+
+});
